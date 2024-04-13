a8dd324dac28aee17acffea0b640665a801ff05d
trantronghieu222 <tranhieu4345@gmail.com>
2024-04-13 22:13:37 +0700

    cập nhật model

--- customer/model/cart.js
+++ customer/model/cart.js
@@ -1,6 +1,17 @@
-function cart (_name, _price, _screen, _quan){
-    this.name = _name;
-    this.price = _price;
-    this.screen = _screen;
-    this.quan = _quan;
+// function cart (products , quantity){
+//     this.products = products;
+//     this.quantity = quantity;
+// }
+
+function cart (name, price, screen, backCamera, frontCamera, img, desc, type, id, quantity){
+    this.name = name;
+    this.price = price;
+    this.screen = screen;
+    this.backCamera = backCamera;
+    this.frontCamera = frontCamera;
+    this.img = img;
+    this.desc = desc;
+    this.type = type;
+    this.id = id;
+    this.quantity = quantity;
 }
